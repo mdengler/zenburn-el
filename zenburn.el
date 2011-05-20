21880ff0db610bbe0fa8f54bd2837b8bc75edd98
Martin Dengler <martin@martindengler.com>
2011-05-21 03:02:46 +0800

    import rainbow-delimiters from djcb's emacs-24 zenburn theme

--- zenburn.el
+++ zenburn.el
@@ -755,6 +755,20 @@ static char *gnus-pointer[] = {
           '(outline-2 ((t (:inherit outline-3 :height 1.0))))
           '(outline-1 ((t (:inherit outline-2 :height 1.0))))
 
+          ; rainbow-delimiters
+          `(rainbow-delimiters-depth-1-face ((t (:foreground ,zenburn-cyan))))
+          `(rainbow-delimiters-depth-2-face ((t (:foreground ,zenburn-yellow))))
+          `(rainbow-delimiters-depth-3-face ((t (:foreground ,zenburn-blue+1))))
+          `(rainbow-delimiters-depth-4-face ((t (:foreground ,zenburn-red+1))))
+          `(rainbow-delimiters-depth-5-face ((t (:foreground ,zenburn-orange))))
+          `(rainbow-delimiters-depth-6-face ((t (:foreground ,zenburn-blue-1))))
+          `(rainbow-delimiters-depth-7-face ((t (:foreground ,zenburn-green+4))))
+          `(rainbow-delimiters-depth-8-face ((t (:foreground ,zenburn-red-3))))
+          `(rainbow-delimiters-depth-9-face ((t (:foreground ,zenburn-yellow-2))))
+          `(rainbow-delimiters-depth-10-face ((t (:foreground ,zenburn-green+2))))
+          `(rainbow-delimiters-depth-11-face ((t (:foreground ,zenburn-blue+1))))
+          `(rainbow-delimiters-depth-12-face ((t (:foreground ,zenburn-red-4))))
+
           ; rcirc
           '(rcirc-my-nick ((t (:inherit zenburn-primary-1))))
           '(rcirc-other-nick ((t (:inherit bold))))
